59b29b3c75425b55fdc6dccedfd89edd00008298
monpeco <monpeco@gmail.com>
2016-12-31 04:08:26 +0000

    Execute Scaffold Article (title, post, user_id) and rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161231033102) do
+ActiveRecord::Schema.define(version: 20161231040456) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.text     "post",       limit: 65535
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "password",   limit: 255
