1fe47726e9619ea60f67677cf11789375d325378
monpeco <monpeco@gmail.com>
2017-01-01 17:45:06 +0000

    Add validation on article and user

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ActiveRecord::Base
+    validates :title, presence: true,
+                length: {maximum: 50}
+    validates :post, presence: true               
+                
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+    validates :password, presence: true,
+                length: { minimum: 8}
+    validates :email, presence: true
+    
 end
