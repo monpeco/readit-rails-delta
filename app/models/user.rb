faae9fc77f8924e0a04ec67da70bce3e5a6a192e
monpeco <monpeco@gmail.com>
2017-01-04 13:27:38 +0000

    Add before_save check to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,18 @@
 class User < ActiveRecord::Base
+    before_save :check_stock #
+
     has_many :articles
     validates :password, presence: true,
                 length: { minimum: 8}
     validates :email, presence: true
+
+
+    def check_stock #
+       
+       if self.password == '12345678'
+         errors[:attribute] << "No se puede usar password tan facil"
+         return false
+       end
+    end
     
 end
